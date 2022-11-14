8aa895207fd2c12d9b441e6bb86c275e18359ce8
alumno <alumno@A301PC16>
2022-11-14 11:56:25 +0100

    Agregar archivos de proyecto.

---
+++ Comida/ComidaVM.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comida
+{
+    class ComidaVM : INotifyCollectionChanged, INotifyPropertyChanged
+    {
+        private ObservableCollection<Plato> platos;
+        private Plato platoSeleccionado;
+        
+
+        public Plato PlatoSeleccionado
+        {
+            get { return platoSeleccionado; }
+            set { 
+                platoSeleccionado = value;
+                NotifyPropertyChanged("PlatoSeleccionado");
+            }
+        }
+
+
+        public ObservableCollection<Plato> Platos
+        {
+            get { return platos; }
+            set { 
+                platos = value;
+                NotifyPropertyChanged("Platos");
+            }
+        }
+
+        public ComidaVM()
+        {
+            Platos = new ObservableCollection<Plato>();
+            Platos = Plato.GetSamples("recursos/");
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ Comida/Plato.cs
@@ -0,0 +1,123 @@
+﻿
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+
+class Plato : INotifyPropertyChanged
+{
+
+    private string nombre;
+    private string imagen;
+    private string tipo;
+    private bool gluten;
+    private bool soja;
+    private bool leche;
+    private bool sulfitos;
+
+    public string Nombre
+    {
+        get { return nombre; }
+
+        set
+        {
+            nombre = value;
+            NotifyPropertyChanged("Nombre");
+        }
+    }
+    public string Imagen { 
+        
+        get { return imagen; }
+        set
+        {
+            imagen=value;
+            NotifyPropertyChanged("Imagen");
+        }
+    }
+    public string Tipo
+    {
+
+        get { return tipo; }
+        set
+        {
+            tipo = value;
+            NotifyPropertyChanged("Tipo");
+        }
+    }
+
+    public bool Gluten
+    {
+
+        get { return gluten; }
+        set
+        {
+            gluten = value;
+            NotifyPropertyChanged("Gluten");
+        }
+    }
+    public bool Soja
+    {
+
+        get { return soja; }
+        set
+        {
+            soja = value;
+            NotifyPropertyChanged("Soja");
+        }
+    }
+    public bool Leche
+    {
+
+        get { return leche; }
+        set
+        {
+            leche = value;
+            NotifyPropertyChanged("Leche");
+        }
+    }
+    public bool Sulfitos
+    {
+
+        get { return sulfitos; }
+        set
+        {
+            sulfitos = value;
+            NotifyPropertyChanged("Sulfitos");
+        }
+    }
+
+    public Plato(string nombre, string imagen, string tipo, bool gluten, bool soja, bool leche, bool sulfitos)
+    {
+        Nombre = nombre;
+        Imagen = imagen;
+        Tipo = tipo;
+        Gluten = gluten;
+        Soja = soja;
+        Leche = leche;
+        Sulfitos = sulfitos;
+    }
+
+    public Plato()
+    {
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void NotifyPropertyChanged(string propertyName)
+    {
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public static ObservableCollection<Plato> GetSamples(string rutaImagenes)
+    {
+        ObservableCollection<Plato> lista = new ObservableCollection<Plato>();
+
+        lista.Add(new Plato("Hamburguesa", Path.Combine(rutaImagenes, @"burguer.jpg"), "Americana", true, false, true, true));
+        lista.Add(new Plato("Dumplings", Path.Combine(rutaImagenes, @"dumplings.jpg"), "China", true, true, false, false));
+        lista.Add(new Plato("Tacos", Path.Combine(rutaImagenes, @"tacos.jpg"), "Mexicana", true, false, false, true));
+        lista.Add(new Plato("Cerdo agridulce", Path.Combine(rutaImagenes, @"cerdoagridulce.jpg"), "China", true, true, false, true));
+        lista.Add(new Plato("Hot dogs", Path.Combine(rutaImagenes, @"hotdog.jpg"), "Americana", true, true, true, true));
+        lista.Add(new Plato("Fajitas", Path.Combine(rutaImagenes, @"fajitas.jpg"), "Mexicana", true, false, false, true));
+
+        return lista;
+    }
+}
+
